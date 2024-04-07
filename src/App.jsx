76ddf24f02905e62cfce986909d28eadf770c27a
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-04-07 18:49:36 +0100

    Finished basic preparations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
+import Loader from "./components/Loader/Loader.jsx";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
+const Navigation = lazy(() => import("./components/Navigation/Navigation.jsx"));
+const NotFoundPage = lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
 
 function App() {
   return (
     <>
-      <div>app</div>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+
+            <Route path="/*" element={<NotFoundPage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+
+            <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+          </Routes>
+        </Suspense>
+      </main>
     </>
   );
 }
